3657706eefe2112f72c838db382eafc16b0313b0
maicao1 <maicon.carolino.campos@escola.pr.gov.br>
2026-02-23 09:12:11 -0300

    Add files via upload

---
+++ Jokenpo 2 Prof Edson/jokenpo.js
@@ -0,0 +1,36 @@
+function jogar(){
+
+    if (document.getElementById('pedra').checked == false && 
+    document.getElementById('papel').checked == false &&
+    document.getElementById('tesoura').checked == false){
+
+        alert("Selecione uma função")
+
+    }
+
+    else {
+
+        let sorteio = Math.floor(Math.random()*3);
+
+        switch(sorteio){
+            
+            case 0 : 
+            document.getElementById("pc").src="pcpedra.png";
+            break;
+
+            case 1:
+                document.getElementById("pc").src="pcpapel.png";
+
+                break;
+
+
+                case 2:
+                    document.getElementById("pc").src="pctesoura.png";
+                    break;
+
+        }
+        if((document.getElementById("pedra").checked == true && sorteio == 0)
+            ||(document.getElementById("papel")checked == true && sorteio == 1)||())
+
+    }
+}
